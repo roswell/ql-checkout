607b73cfbe28cd4dc4cb77f50456a207debf1c95
SANO Masatoshi <snmsts@gmail.com>
2018-12-20 11:05:40 +0800

    mercurial

--- ql-checkout.asd
+++ ql-checkout.asd
@@ -1,7 +1,9 @@
 ;;don't edit
 (DEFSYSTEM "ql-checkout" :LICENSE "mit" :CLASS :PACKAGE-INFERRED-SYSTEM
  :COMPONENTS
- ((:FILE "config") (:FILE "github") (:FILE "meta-project") (:FILE "vcs")
-  (:FILE "project-quicklisp") (:FILE "project-ultralisp") (:FILE "vcs-git")
-  (:FILE "vcs-svn") (:FILE "main"))
+ ((:FILE "config") (:FILE "github") (:FILE "meta-project")
+  (:FILE "vcs") (:FILE "project-quicklisp") (:FILE "project-ultralisp")
+  (:FILE "vcs-git") (:FILE "vcs-svn") (:FILE "vcs-mercurial")
+  (:FILE "main"))
+ :DESCRIPTION "ql-checkout is library intend to checkout quicklisp maintained library with vcs."
  :AUTHOR "SANO Masatoshi" :MAILTO "snmsts@gmail.com")
---
+++ vcs-mercurial.lisp
@@ -0,0 +1,19 @@
+(uiop/package:define-package :ql-checkout/vcs-mercurial (:use :cl :ql-checkout/vcs))
+(in-package :ql-checkout/vcs-mercurial)
+;;;don't edit above
+
+(defclass mercurial (vcs) ())
+
+(defmethod vcs-init ((vcs (eql 'mercurial)) params)
+  (make-instance vcs :uri (first params)))
+(register-vcs 'mercurial)
+
+(defmethod vcs-checkout ((vcs mercurial) directory quiet)
+  (let ((dir (vcs-dir vcs directory)))
+    (unless quiet
+      (format t "hg clone ~A~%" dir))
+    (uiop:run-program (format nil "hg clone ~A ~A"
+                              (vcs-uri vcs)
+                              (namestring
+                               (ensure-directories-exist dir))))
+    dir))
