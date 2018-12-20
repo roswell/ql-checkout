86af97929d414b05da49b834c4abae4545840ca7
SANO Masatoshi <snmsts@gmail.com>
2018-12-20 10:59:16 +0800

    add update in meta-project

--- meta-project.lisp
+++ meta-project.lisp
@@ -1,5 +1,5 @@
 (uiop/package:define-package :ql-checkout/meta-project (:nicknames) (:use :ql-checkout/github :cl)
-                             (:shadow) (:export :clone-github :project :register-project :find-source) (:intern))
+                             (:shadow) (:export :clone-github :project :register-project :find-source :update) (:intern))
 (in-package :ql-checkout/meta-project)
 ;;don't edit above
 
@@ -14,3 +14,6 @@
 (defun register-project (name)
   (unless (find-if (lambda (x) (typep x name)) *projects*)
     (push (make-instance name) *projects*)))
+
+(defmethod update ((project project))
+  (error "not supported project ~S" project))
